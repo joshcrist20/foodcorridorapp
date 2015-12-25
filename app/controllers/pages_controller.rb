adfb9192646c72141960683a9b42b8fe08017989
Josh Crist <joshcrist20@gmail.com>
2015-12-25 13:15:34 -0700

    Created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dashboard]
   def home
   	if current_user
   		redirect_to kitchens_path
 	end
   end
+
+	def dashboard
+		@kitchens = current_user.kitchens
+	end
 end
